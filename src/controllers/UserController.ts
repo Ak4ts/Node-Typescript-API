647ff0093b63175e3409bcfa188e6a4c34d46285
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-02-12 23:03:58 -0300

    feat: added index function

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,5 +3,9 @@ import { Request, Response} from "express"
 
 class UserController {
 
+  public async index(req: Request, res: Response): Promise<Response>{
+    const users = await User.find()
+    return res.status(200).json(users)
+  }
 
 export default new UserController()
