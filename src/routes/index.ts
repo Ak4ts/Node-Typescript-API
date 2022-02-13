864155c67fb10bfdf099713c83d9474933cb332e
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-02-13 16:40:12 -0300

    feat: added login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,4 +8,6 @@ routes.get("/api/users", UserController.index)
 routes.post("/api/users", UserController.create);
 routes.delete("/api/users",UserMiddleware.verifyToken, UserController.deleteUser)
 
+routes.post("/api/login", UserController.login, UserMiddleware.verifyToken , UserController.index);
+
 export default routes
