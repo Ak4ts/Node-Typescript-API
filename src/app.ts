7d55672d52a0452184c85f4323b6332347564656
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-02-12 23:03:04 -0300

    feat: added express constructor

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ import mongoose from "mongoose";
 import routes from "./routes";
 
 class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+  }
 }
 
 export default new App().express
