00e4cc0cbf9e44cce2b285c320c5adc3b1fa0dd9
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-02-13 16:39:22 -0300

    feat: created delete route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import UserMiddleware from "../middlewares/UserMiddleware"
 
 const routes = Router()
 
 routes.get('/api/users', UserController.index)
 routes.post("/api/users", UserController.create);
+routes.delete("/api/users",UserMiddleware.verifyToken, UserController.deleteUser)
 
 export default routes
