3b915217614d69dc201b8bb86cea43d2984de073
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-02-12 23:03:47 -0300

    feat: added middleware function

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,15 @@ class App {
 
   public constructor() {
     this.express = express();
+
+    this.middlewares()
     this.database()
     this.routes()
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
   }
   private database(): void {
     mongoose.connect("mongodb://localhost:27017/ts");
